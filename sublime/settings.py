faf5848cd4b12d677ab901ae022254791889daeb
joseph <sublimeboma1@gmail.com>
2026-08-19 17:43:55 +0200

    YUTY

--- sublime/settings.py
+++ sublime/settings.py
@@ -13,6 +13,7 @@ import pymysql
 pymysql.install_as_MySQLdb()
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -96,27 +97,23 @@ CACHES = {
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}"""
+
+
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 
-"""DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'maspboma$sublimeboma',
-        'USER': 'maspboma',
-        'PASSWORD': 'jos123@#',
-        'HOST': 'maspboma.mysql.pythonanywhere-services.com',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
-    }
-}"""
 
 
 # Password validation
