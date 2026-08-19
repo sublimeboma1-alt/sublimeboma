8948dac06bc4be9a519d9f08bf2097edf33c15d3
joseph <sublimeboma1@gmail.com>
2026-08-19 18:29:59 +0200

    YUTY

--- sublime/settings.py
+++ sublime/settings.py
@@ -94,21 +94,11 @@ CACHES = {
     }
 }
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-"""DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}"""
-
 
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('Postgres.DATABASE_PRIVATE_URL'),
+        default=os.environ.get('DATABASE_PUBLIC_URL'),
         conn_max_age=600
     )
 }
