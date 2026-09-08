a94b7c9131ef53da979f88e36d8049c10cba102e
joseph <sublimeboma1@gmail.com>
2026-09-08 11:47:05 +0200

    RODE

--- sublime/settings.py
+++ sublime/settings.py
@@ -13,7 +13,7 @@ import pymysql
 pymysql.install_as_MySQLdb()
 from pathlib import Path
 import os
-import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
